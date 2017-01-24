3bd4438320ec2f30a89d2ea15099b96470ae2e7d
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-24 11:28:43 -0800

    Flesh out authorize function

--- quickstart.js
+++ quickstart.js
@@ -3,4 +3,13 @@ var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
+  oauth2Client.credentials = JSON.parse(token);
+  callback(oauth2Client);
+}
