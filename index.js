5cf3c06192f51b2eb4c621fd64e3e11210d286fc
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2017-01-24 18:21:24 -0800

    Fetch events from calender that match todays date

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 var gCal = require('google-calendar');
 var auth = require('./auth.js');
 
-
+var nowDate = new Date("January 17, 2017 11:13:00");
+var tomorowDate = new Date(nowDate.getTime() + 86400000);
+var today = getDMY(nowDate);
+var tomorow = getDMY(tomorowDate);
 
 var calendar = new gCal.GoogleCalendar(auth.googleToken);
 
+var options = {
+  'orderBy': 'startTime', 
+  'singleEvents': true, 
+  'timeMax': `${tomorow[2]}-${tomorow[1]}-${tomorow[0]}T00:00:00-07:00`, 
+  'timeMin': `${today[2]}-${today[1]}-${today[0]}T00:00:00-07:00`
+};
 
-var parseCalenderList = function parseCalenderData(data) {
+
+calendar.events.list('hir.7@hackreactor.com', options, function(err, calendarList) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(parseCalenderList(calendarList));
+});
+
+
+function getDMY(dateObj) {
+  var day = dateObj.getDate();
+  if (day < 10) {day = '0' + day;}
+  var month = dateObj.getMonth() + 1;
+  if (month < 10) {month = '0' + month;}
+  var year = dateObj.getFullYear();
+
+  return [day, month, year];
+}
+
+function parseCalenderList(data) {
   var slots = [];
   var interviews = [];
   data.items.forEach(function(event) {
@@ -17,15 +45,4 @@ var parseCalenderList = function parseCalenderData(data) {
     }
   });
   return [slots, interviews];
-};
-
-
-calendar.events.list('hir.7@hackreactor.com', {'orderBy': 'startTime', 'singleEvents': true, 'timeMax': '2017-01-24T00:00:00-00:00', 'timeMin': '2017-01-23T00:00:00-00:00'}, function(err, calendarList) {
-  if (err) {
-    return console.log(err);
-  }
-  //console.log(parseCalenderList(calendarList));
-});
-
-
-
+}
