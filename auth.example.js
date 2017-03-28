3730ef714900ecf9ea4837455a9fa734cd677e9b
Dan Gilday <dangitday@gmail.com>
2017-03-28 14:26:17 -0700

    DVG - Update Lead HiR and Email Targets

--- auth.example.js
+++ auth.example.js
@@ -1,7 +1,7 @@
 module.exports = {
   'googleRefreshToken': 'REFRESH_TOKEN',
-  'client_id':          'CLIENT_ID', 
-  'client_secret':      'CLIENT_SECRET',
-  'gmailUsername':      'UERNAME',
-  'gmailPassword':      'PASSWORD'
+  'client_id': 'CLIENT_ID', 
+  'client_secret': 'CLIENT_SECRET',
+  'gmailUsername': 'UERNAME',
+  'gmailPassword': 'PASSWORD'
 };
--- constants.js
+++ constants.js
@@ -1,20 +1,22 @@
 module.exports = {
   hirs: {
-    '1' : ['Bill', 'hir.1@hackreactor.com', true],
-    '2' : ['Robin', 'hir.2@hackreactor.com', true],
-    '7' : ['Dylan', 'hir.7@hackreactor.com', true],
-    '8' : ['Hans', 'hir.8@hackreactor.com', true],
-//The boolean refers to if the HiR is a senior and available to do Mock Interviews
-    '4' : ['Zach', 'hir.4@hackreactor.com', false],
-    '5' : ['Savaughn', 'hir.5@hackreactor.com', false],
-    '6' : ['Ethan', 'hir.6@hackreactor.com', false],
-    '10': ['Dan', 'hir.10@hackreactor.com', false],
-    '11': ['Susan', 'hir.11@hackreactor.com', false],
-    '12': ['Autumn', 'hir.12@hackreactor.com', false]
+    '4': ['Cynthia', 'hir.4@hackreactor.com', false],
+    '5': ['Anthony', 'hir.5@hackreactor.com', false],
+    '10': ['Aaron', 'hir.10@hackreactor.com', false],
+    '11': ['Kay', 'hir.11@hackreactor.com', false],
+// The boolean refers to if the HiR is a senior and available to do Mock Interviews
+    '1': ['Zack', 'hir.1@hackreactor.com', true],
+    '2': ['Savaughn', 'hir.2@hackreactor.com', true],
+    '3': ['Ethan', 'hir.3@hackreactor.com', true],
+    '6': ['Dan', 'hir.6@hackreactor.com', true],
+    '7': ['Susan', 'hir.7@hackreactor.com', true],
+    '8': ['Autumn', 'hir.8@hackreactor.com', true],
+    '9': ['George', 'hir.9@hackreactor.com', true]
   }, 
 
-  signiture: 'Dylan Larrabee',
-  email: 'dylan.larrabee@hackreactor.com',
-  team: ['sfm.technical.mentors.team@hackreactor.com', 'sfm.counselors.team@hackreactor.com', 'sfm.coordinators.team@hackreactor.com', 'sfm.hir.team@hackreactor.com'],
+  signiture: 'Dan Gilday',
+  email: 'dan.gilday@hackreactor.com',
+  team: ['sfm.instruction.team@hackreactor.com', 'sfm.hir.team@hackreactor.com'],
+  // ['sfm.technical.mentors.team@hackreactor.com', 'sfm.counselors.team@hackreactor.com', 'sfm.coordinators.team@hackreactor.com', 'sfm.hir.team@hackreactor.com'],
 
 };
