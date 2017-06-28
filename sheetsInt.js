9294e983b1e7c7ead4df90d3962d5f3e77a242d0
Erik Thompson <erik.james.thompson@gmail.com>
2017-06-27 19:00:57 -0700

    gSheets partially set up

---
+++ sheetsInt.js
@@ -0,0 +1,8 @@
+var auth = require('./auth.js');
+// var signiture = people.signiture;
+var people = require('./constants.js');
+var gSheet = require('google-spreadsheet')
+
+module.exports = {
+  var doc = new gSheet(auth.googleSpreadsheetKey)
+}
